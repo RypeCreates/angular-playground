75855156f43209b6c935d4746d9dca1df665df74
Ryan Petit <petit.ryank@gmail.com>
2022-01-17 22:09:49 -0500

    defaulted playground app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WheelComponent } from './wheel/wheel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WheelComponent
   ],
   imports: [
     BrowserModule,
